5a3104d654e2dd8809b35a2c19faefe96a640a42
Saeed Gholami <saeed.ghollami@gmail.com>
2024-08-26 03:41:53 +0330

    Make tkinter checkbutton group

--- quiz_app/tkui.py
+++ quiz_app/tkui.py
@@ -10,25 +10,44 @@ class MainFrame(ttk.Frame):
 
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
+        # an option(checkbutton) selected (checked)
+        self.option_checked = False
+        self.option_checked_index = -1
+        # get questions from json file
         self.questions = read_questions("questions.json")
-        self.question_var = tk.StringVar()
-        self.answer_var = tk.IntVar()
-        self.options_var = []
 
-        q0 = self.questions[MainFrame.question_index]
-        self.question_var.set(q0["question"])
-        ttk.Label(self, textvariable=self.question_var).pack(anchor="w")
-        options = q0.get("options")
-        for option in options:
-            opt_var = tk.BooleanVar()
-            self.options_var.append(opt_var)
-            ttk.Checkbutton(self, text=option, variable=opt_var).pack(anchor="w")
-        
+        # Variables
+        self.question_var = tk.StringVar()
+        self.option_0_var = tk.BooleanVar()
+        self.option_1_var = tk.BooleanVar()
+        self.option_2_var = tk.BooleanVar()
+        self.option_3_var = tk.BooleanVar()
+        self.options_var = [
+            self.option_0_var,
+            self.option_1_var,
+            self.option_2_var,
+            self.option_3_var,
+        ]
+        self.ans_index = 0
+
+        self.question_label = ttk.Label(self, textvariable=self.question_var)
+        self.option_0 = ttk.Checkbutton(self, variable=self.option_0_var)
+        self.option_1 = ttk.Checkbutton(self, variable=self.option_1_var)
+        self.option_2 = ttk.Checkbutton(self, variable=self.option_2_var)
+        self.option_3 = ttk.Checkbutton(self, variable=self.option_3_var)
+        self.option_0.config(command=self.uncheck_other)
+        self.option_1.config(command=self.uncheck_other)
+        self.option_2.config(command=self.uncheck_other)
+        self.option_3.config(command=self.uncheck_other)
+
+        self.message = ttk.Label(self)
 
         next_button = ttk.Button(self, text="Next")
-        next_button.pack()
+        next_button.pack(anchor="w", pady=10)
         next_button.config(command=self.next_question)
 
+        check_ans_button = ttk.Button(self, text="Check", command=self.check_ans)
+        check_ans_button.pack(anchor="w")
 
     def make_question(self, question):
         question = self.questions[MainFrame.question_index]
@@ -38,12 +57,59 @@ class MainFrame(ttk.Frame):
         q_obj = Quiz(question_text, options, answer_index)
         return q_obj
 
-    def next_question(self):
-        question = self.questions[MainFrame.question_index]
-        q = self.make_question(question)
-        ttk.Label(self,  text=q.question).pack()
+    def uncheck_all(self):
+        for var in self.options_var:
+            var.set(value=False)
 
+    def next_question(self, index=0):
+        q = self.questions[MainFrame.question_index]
+        q_text = q.get("question")
+        options = q.get("options")
+        self.ans_index = int(q.get("answer"))
+
+        self.question_var.set(q_text)
+        self.question_label.pack(anchor="w")
+
+        self.option_0.config(text=options[0])
+        self.option_1.config(text=options[1])
+        self.option_2.config(text=options[2])
+        self.option_3.config(text=options[3])
+        self.option_0.pack(anchor="w")
+        self.option_1.pack(anchor="w")
+        self.option_2.pack(anchor="w")
+        self.option_3.pack(anchor="w")
+
+        self.uncheck_all()
+        self.message.config(text="")
         MainFrame.question_index += 1
+
+    def get_selected_index(self, options):
+        if True in options:
+            return options.index(True)
+        return -1
+
+    def check_ans(self):
+        user_ans_index = self.get_selected_index(self.options_var)
+        if self.ans_index == user_ans_index:
+            self.message.config(text="Correct")
+        else:
+            self.message.config(text="Incorrect")
+        self.message.pack(anchor="w")
+
+    def uncheck_other(self):
+        options = [var.get() for var in self.options_var]
+
+
+        if self.option_checked and self.option_checked_index != -1:
+            alredy_checked_option = self.options_var[self.option_checked_index]
+            alredy_checked_option.set(value=False)
+            options[self.option_checked_index] = False
+            self.option_checked_index = self.get_selected_index(options)
+        else:
+            # store first time checked checkbutton index
+            self.option_checked_index = self.get_selected_index(options)
+            self.option_checked = True
+
 
 
 class App(tk.Tk):
